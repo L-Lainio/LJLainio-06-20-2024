3274849afa4e1547060923ef3fb64b037113d8b9
L-Lainio <arollainio@gmail.com>
2024-06-21 13:31:09 -0400

    updated app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Navigation from './components/Navigation'
+import Project from './components/Project'
+
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+
+
+
+function App() {
+    return (
+        <main className="App">
+            <Header />
+            <hr />
+            <Navigation />
+            <hr />
+            <Project />
+            <hr />
+            <Footer />
+        </main>
+    )
+}
+
+
+export default App
